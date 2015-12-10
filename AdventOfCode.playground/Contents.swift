29b22962b5fa1f1c7cfc84559d6d794f1b5dc4ee
samisuteria <sami.suteria@gmail.com>
2015-12-09 16:46:51 -0800

    Day 1 Solutions

--- AdventOfCode.playground/Contents.swift
+++ AdventOfCode.playground/Contents.swift
@@ -1,5 +1,49 @@
-//: Playground - noun: a place where people can play
+import Foundation
 
-import UIKit
+//Reads a file and returns the contents as a String
+//Error if file not found or decoding
+func readFile(fileName: String) -> String {
+    let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "txt")
+    let fileData = NSFileManager.defaultManager().contentsAtPath(filePath!)
+    let fileContent = NSString(data: fileData!, encoding: NSASCIIStringEncoding)
+        
+    return String(fileContent!)
+}
 
-var str = "Hello, playground"
+//Day 1
+
+func Day1Part1(input: String) -> Int {
+    var floor = 0
+    
+    input.characters.forEach { (char) -> () in
+        if char == "(" {
+            floor++
+        } else if char == ")" {
+            floor--
+        }
+    }
+    
+    return floor
+}
+
+func Day1Part2(input: String) -> Int {
+    var floor = 0
+    
+    for (index, char) in input.characters.enumerate() {
+        if char == "(" {
+            floor++
+        } else if char == ")" {
+            floor--
+        }
+        
+        if floor < 0 {
+            return index + 1 //since the arrays on the site start 1
+        }
+    }
+    
+    return 0 //error
+}
+
+let day1String = readFile("Day1")
+Day1Part1(day1String)
+Day1Part2(day1String)
