67eed7915c8ae8e4004875163f86235d3f4edc82
samisuteria <sami.suteria@gmail.com>
2015-12-10 12:08:40 -0800

    Day 3 Solutions

--- AdventOfCode.playground/Contents.swift
+++ AdventOfCode.playground/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 func readFile(fileName: String) -> String {
     let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "txt")
     let fileData = NSFileManager.defaultManager().contentsAtPath(filePath!)
-    let fileContent = NSString(data: fileData!, encoding: NSASCIIStringEncoding)
+    let fileContent = NSString(data: fileData!, encoding: NSUTF8StringEncoding)
         
     return String(fileContent!)
 }
@@ -48,16 +48,14 @@ func Day1Part2(input: String) -> Int {
 //Day1Part1(day1String)
 //Day1Part2(day1String)
 
-struct Dimensions {
-    let length: Int
-    let width: Int
-    let height: Int
-}
+//Day 2
+
+typealias Dimensions = (length: Int, width: Int, height: Int)
 
 func Day2StringToDimensions(input: String) -> [Dimensions] {
     var presents = [Dimensions]()
     
-    let listOfPresentDimensions = day2String.componentsSeparatedByString("\n")
+    let listOfPresentDimensions = input.componentsSeparatedByString("\n")
     
     listOfPresentDimensions.forEach { (presentString) -> () in
         let presentValues = presentString.componentsSeparatedByString("x")
@@ -89,7 +87,8 @@ func Day2Part1(input: String) -> Int {
 }
 
 func Day2Part2(input: String) -> Int {
-    let presents = Day2StringToDimensions(day2String)
+    
+    let presents = Day2StringToDimensions(input)
     
     var total = 0
     
@@ -105,6 +104,98 @@ func Day2Part2(input: String) -> Int {
     return total
 }
 
-let day2String = readFile("Day2")
-Day2Part1(day2String)
-Day2Part2(day2String)
+//let day2String = readFile("Day2")
+//Day2Part1(day2String)
+//Day2Part2(day2String)
+
+//Day 3
+
+struct House: Hashable {
+    let x: Int
+    let y: Int
+    
+    var hashValue : Int {
+        get {
+            return "\(self.x),\(self.y)".hashValue
+        }
+    }
+}
+
+func == (lhs: House, rhs: House) -> Bool {
+    return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+}
+
+func Day3Part1(input: String) -> Int {
+    var HousesVisited = Set<House>()
+    
+    var currentHouse = House(x: 0, y: 0)
+    HousesVisited.insert(currentHouse)
+    
+    input.characters.forEach { (char) -> () in
+        
+        switch char {
+        case "^":
+            currentHouse = House(x: currentHouse.x, y: currentHouse.y + 1)
+        case "v":
+            currentHouse = House(x: currentHouse.x, y: currentHouse.y - 1)
+        case ">":
+            currentHouse = House(x: currentHouse.x + 1, y: currentHouse.y)
+        case "<":
+            currentHouse = House(x: currentHouse.x - 1, y: currentHouse.y)
+        default:
+            break
+        }
+        
+        HousesVisited.insert(currentHouse)
+    }
+    
+    return HousesVisited.count
+}
+
+func Day3Part2(input: String) -> Int {
+    var HousesVisited = Set<House>()
+    
+    var santaCurrentHouse = House(x: 0, y: 0)
+    var roboSantaCurrentHouse = House(x: 0, y: 0)
+    var currentHouse = House(x: 0, y: 0)
+    
+    HousesVisited.insert(currentHouse)
+    
+    var santasTurn = true
+    
+    input.characters.forEach { (char) -> () in
+        
+        currentHouse = santasTurn ? santaCurrentHouse : roboSantaCurrentHouse
+        
+        switch char {
+        case "^":
+            currentHouse = House(x: currentHouse.x, y: currentHouse.y + 1)
+        case "v":
+            currentHouse = House(x: currentHouse.x, y: currentHouse.y - 1)
+        case ">":
+            currentHouse = House(x: currentHouse.x + 1, y: currentHouse.y)
+        case "<":
+            currentHouse = House(x: currentHouse.x - 1, y: currentHouse.y)
+        default:
+            break
+        }
+        
+        HousesVisited.insert(currentHouse)
+        
+        if santasTurn {
+            santaCurrentHouse = currentHouse
+        } else {
+            roboSantaCurrentHouse = currentHouse
+        }
+        
+        santasTurn = !santasTurn
+    }
+    
+    return HousesVisited.count
+}
+
+//let day3String = readFile("Day3")
+//Day3Part1(day3String)
+//Day3Part2(day3String)
+
+
